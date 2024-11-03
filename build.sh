d3b661452629e89d63ab510f48d845ad5309b8c4
Coconop <Coconop_has_no_m@il.com>
2024-11-03 12:55:10 +0100

    Fix ToC

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-rm out/* && pdflatex --output-directory=out/ *.tex && mv out/*.pdf pdf/
+# Compile twice for ToC
+rm out/* ||\
+pdflatex --output-directory=out/ *.tex &&\
+pdflatex --output-directory=out/ *.tex &&\
+mv out/*.pdf pdf/
